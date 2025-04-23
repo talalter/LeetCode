7d28c72887ae6c14a985931b0c49cb4a0455646f
talalter <64363268+talalter@users.noreply.github.com>
2025-04-23 21:34:46 +0300

    Time: 244 ms (65.14%) | Memory: 20.3 MB (43.58%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,25 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def dfs(r, c):
-            if r < 0 or r >= rows or c < 0 or c >= cols:
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == '0':
                 return
-                
-            visited.add((r,c))
 
-            if grid[r][c] == '0':
-                return
+            grid[r][c] = '0'
 
             for dr, dc in directions:
-                if (r + dr, c+dc) not in visited:
-                    dfs(r + dr, c+dc)
+                dfs(r + dr, c+dc)
 
         rows, cols = len(grid), len(grid[0])
-        visited = set()
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         islands = 0
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1' and (r, c) not in visited:
+                if grid[r][c] == '1':
                     dfs(r, c)
                     islands += 1
 
