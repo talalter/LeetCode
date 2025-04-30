519dd0a3dd73260a0aad0cd26788a5b01d1a606c
talalter <64363268+talalter@users.noreply.github.com>
2025-04-30 19:00:23 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (16.11%) - LeetSync

--- 163-missing-ranges/missing-ranges.py
+++ 163-missing-ranges/missing-ranges.py
@@ -3,22 +3,20 @@ class Solution:
  
 
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
-        def format_range(start: int, end: int) -> str:
-            if start == end:
-                return [start, start]
-            else:
-                return [start, end]
-        result = []
-        prev = lower - 1  # start from one less than lower
 
+        prev = lower - 1  # start from one less than lower
+        res = []
         # Go through nums and one step beyond to capture the upper bound
         for i in range(len(nums) + 1):
             # Set curr to nums[i] if i is valid, otherwise set it to upper + 1
             curr = nums[i] if i < len(nums) else upper + 1
 
             if curr - prev >= 2:
-                result.append(format_range(prev + 1, curr - 1))
+                if prev + 1 == curr - 1:
+                    res.append([prev + 1, prev + 1])
+                else:
+                    res.append([prev + 1, curr - 1])
 
             prev = curr
 
-        return result
+        return res
