1e9981f51d12c62a2e844fe5fcb6c6fd526a55b4
talalter <64363268+talalter@users.noreply.github.com>
2025-05-15 23:57:37 +0300

    Time: 23 ms (22.03%) | Memory: 18.5 MB (63.70%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.py
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.py
@@ -3,21 +3,18 @@ class Solution:
 
         rows = len(mat)
         cols = len(mat[0])
-        ones_per_rows = [0] * rows
+        max_ones = 0
+        current_index = 0
         for row in range(rows):
             ones = 0
             for col in range(cols):
                 if mat[row][col] == 1:
                     ones += 1
-            ones_per_rows[row] = ones
-        current_max = 0
-        current_index = 0
-        for idx, amount in enumerate(ones_per_rows):
-            if amount > current_max:
-                current_max = amount
-                current_index = idx
+            if ones > max_ones:
+                max_ones = ones
+                current_index = row
 
-        return [current_index, current_max]
+        return [current_index, max_ones]
 
 
         
