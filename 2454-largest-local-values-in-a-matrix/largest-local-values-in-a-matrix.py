ff5c55a0c981ad63f4700dbc3fa930de5caa5b50
talalter <64363268+talalter@users.noreply.github.com>
2025-05-15 20:30:06 +0300

    Time: 51 ms (52.66%) | Memory: 18.6 MB (36.12%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
@@ -8,8 +8,7 @@ class Solution:
                 max_num = 0
                 for dr in range(row, row+3):
                     for dc in range(col, col+3):
-                        if 0 <= dr < n and 0 <= dc < n:
-                            max_num = max(max_num, grid[dr][dc])
+                        max_num = max(max_num, grid[dr][dc])
 
                 res[row][col] = max_num
 
