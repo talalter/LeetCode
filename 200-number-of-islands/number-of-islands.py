9248ba4fafef3e97ba840d18fddc66eaa70c9482
talalter <64363268+talalter@users.noreply.github.com>
2025-05-17 16:18:01 +0300

    Time: 238 ms (77.52%) | Memory: 20 MB (82.68%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,39 +1,22 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def bfs(r, c):
-            queue = deque([(r, c)])
-            grid[r][c] = '0'  # Mark as visited
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == '0':
+                return
 
-            while queue:
-                row, col = queue.popleft()
+            grid[r][c] = '0'
 
-                for dr, dc in directions:
-                    new_r, new_c = row + dr, col + dc
-
-                    if 0 <= new_r < rows and 0 <= new_c < cols and grid[new_r][new_c] == '1':
-                        grid[new_r][new_c] = '0'  # Mark as visited
-                        queue.append((new_r, new_c))
+            for dr, dc in directions:
+                dfs(r + dr, c+dc)
 
         rows, cols = len(grid), len(grid[0])
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         islands = 0
-
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1':
+                    dfs(r, c)
                     islands += 1
-                    bfs(r, c)
 
         return islands
-
-# Example usage:
-grid = [
-    ["1", "1", "0", "0", "0"],
-    ["1", "1", "0", "0", "0"],
-    ["0", "0", "1", "0", "0"],
-    ["0", "0", "0", "1", "1"]
-]
-print(Solution().numIslands(grid))  # Output: 3
+        
