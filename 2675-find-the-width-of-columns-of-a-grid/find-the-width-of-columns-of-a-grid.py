7cf562d889a3dc6c864d30cac74962aa3b7e2be3
talalter <64363268+talalter@users.noreply.github.com>
2025-05-18 13:50:52 +0300

    Time: 9 ms (56.77%) | Memory: 19 MB (44.36%) - LeetSync

--- 2675-find-the-width-of-columns-of-a-grid/find-the-width-of-columns-of-a-grid.py
+++ 2675-find-the-width-of-columns-of-a-grid/find-the-width-of-columns-of-a-grid.py
@@ -1,12 +1,13 @@
 class Solution:
     def findColumnWidth(self, grid: List[List[int]]) -> List[int]:
         def count_digits(num: int) -> int:
-            num = abs(num)
-            counter = 0
-            while num > 0:
-                num = num // 10
-                counter += 1
-            return counter
+            return len(str(num))
+            # num = abs(num)
+            # counter = 0
+            # while num > 0:
+            #     num = num // 10
+            #     counter += 1
+            # return counter
         rows, cols = len(grid), len(grid[0])
         res = [1] * cols
         
@@ -14,7 +15,7 @@ class Solution:
             for col in range(cols):
                 num = grid[row][col] 
                 digits = count_digits(num)
-                if num < 0:
-                    digits += 1
+                # if num < 0:
+                #     digits += 1
                 res[col] = max(res[col], digits)
         return res 
