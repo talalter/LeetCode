73aa6247774f610df2f146a70a5ca5a4ef7c3537
talalter <64363268+talalter@users.noreply.github.com>
2025-05-18 20:50:04 +0300

    Time: 3 ms (56.91%) | Memory: 18.1 MB (69.92%) - LeetSync

--- 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
@@ -1,12 +1,11 @@
+from collections import defaultdict
 class Solution:
     def intersection(self, nums: List[List[int]]) -> List[int]:
-        counter = dict()
+        counter = defaultdict(int)
         n = len(nums)
         for row in nums:
             for num in row:
-                if num in counter:
-                    counter[num] += 1
-                else:
-                    counter[num] = 1
+                counter[num] += 1
+
         
         return sorted([num for num, amount in counter.items() if amount == n])
