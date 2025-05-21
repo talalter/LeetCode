d232567906daf9fd93ce76c7d12e77c74d6bec55
talalter <64363268+talalter@users.noreply.github.com>
2025-05-22 01:53:42 +0300

    Time: 23 ms (93.04%) | Memory: 18.1 MB (96.23%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,30 +1,20 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-
-        
-
-        def check_neighbors(row, col):
-            directions = [[0,1],[1,0],[-1,0],[0,-1]]
-            neighbors = 0
-            for dr, dc in directions:
-                nr, nc = row + dr, col + dc
-                bound_row = 0 <= nr < rows
-                bound_col = 0 <= nc < cols
-                if bound_row and bound_col:
-                    if grid[nr][nc] == 1:
-                        neighbors += 1 
-            return neighbors
-
-
-        lands = 0
-        total_neighbors = 0             
-        
         rows = len(grid)
         cols = len(grid[0])
+        perimeter = 0
+
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == 1:
-                    lands += 4
-                    total_neighbors += check_neighbors(row, col)
+                    perimeter += 4
 
-        return lands - total_neighbors
+                    # Check right
+                    if col + 1 < cols and grid[row][col + 1] == 1:
+                        perimeter -= 2
+
+                    # Check down
+                    if row + 1 < rows and grid[row + 1][col] == 1:
+                        perimeter -= 2
+
+        return perimeter
