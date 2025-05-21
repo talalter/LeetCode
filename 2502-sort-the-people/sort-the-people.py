8c99571413a67f7afe48660f532732c7d648cae1
talalter <64363268+talalter@users.noreply.github.com>
2025-05-21 20:28:21 +0300

    Time: 3 ms (81.99%) | Memory: 18.3 MB (31.23%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,7 +1,6 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         height_to_names = {height : name for height, name in zip(heights, names)}
-        heights.sort(key = lambda x: -x)
 
-        return [height_to_names[height] for height in heights]
+        return [height_to_names[height] for height in sorted(heights, key = lambda x: -x)]
  
