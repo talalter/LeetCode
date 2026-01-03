b7f967bc1c54d1fd1688be68beabb439d46a7126
Tal Alter <64363268+talalter@users.noreply.github.com>
2026-01-03 15:29:29 +0200

    Time: 0 ms (100.00%) | Memory: 17.5 MB (96.30%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,29 +1,31 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-        n = len(nums)
-        def binary_search(flag:bool) -> int:
-            left, right = 0, n-1
-            while left<=right:
-                mid = (left + right) //2
-                if flag:
-                    if nums[mid] > 0:
-                        right = mid - 1
-                    else :
-                        left = mid + 1
-                else:
-                    if nums[mid] < 0:
-                        left = mid + 1
-                    else :
-                        right = mid - 1
-                    
-            if flag:
-                return left
-            return right
         
-        negative = binary_search(False)
-        positive = binary_search(True)
-        num_negatives = negative + 1
-        num_positives = n - positive
-        return max(num_negatives, num_positives)
+        def binary_search_positive(nums):
+            left, right = 0, len(nums)-1
+            while left <= right:
+                mid = (left+right)//2
+                if nums[mid] > 0 and (mid == 0 or nums[mid-1] <=0):
+                    return mid
+                if nums[mid] > 0:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            return -1
+        def binary_search_negative(nums):
+            left, right = 0, len(nums)-1
+            while left <= right:
+                mid = (left+right)//2
+                if nums[mid] < 0 and (mid == len(nums)-1 or nums[mid + 1] >= 0):
+                    return mid
+                if nums[mid] < 0:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return -1
+        neg = binary_search_negative(nums) + 1
 
-                
+        pos_idx = binary_search_positive(nums)
+        pos = 0 if pos_idx == -1 else len(nums) - pos_idx  
+
+        return max(neg, pos)
