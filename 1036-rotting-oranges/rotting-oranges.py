69b21b8510e3586fe62531e1da096d1f6dec6f3c
Tal Alter <64363268+talalter@users.noreply.github.com>
2026-01-03 18:43:17 +0200

    Time: 3 ms (81.20%) | Memory: 17.4 MB (86.56%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -6,23 +6,20 @@ class Solution:
         total_minutes = 0
         directions = [(1,0),(-1,0),(0,1),(0,-1)]
         queue = deque()
-        visited = set()
         for row in range(n_rows):
             for col in range(n_cols):
                 if grid[row][col] == 2:
                     queue.append((row, col))
-                    visited.add((row, col))
         while queue:
             rotten_this_round = False
             for _ in range(len(queue)):
                 r, c = queue.popleft()
                 for dr, dc in directions:
                     nr, nc = r + dr, c + dc
-                    if (nr, nc) not in visited and 0 <= nr < n_rows and 0 <= nc < n_cols and grid[nr][nc] == 1:
+                    if 0 <= nr < n_rows and 0 <= nc < n_cols and grid[nr][nc] == 1:
                         rotten_this_round = True
                         grid[nr][nc] = 2
                         queue.append((nr, nc))
-                        visited.add((nr, nc))
             if rotten_this_round:
                 total_minutes += 1 
 
