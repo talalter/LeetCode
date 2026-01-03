abd470207274a8de399cf1da110c9a6abeb4fa8e
Tal Alter <64363268+talalter@users.noreply.github.com>
2026-01-03 17:46:35 +0200

    Time: 244 ms (58.62%) | Memory: 19.4 MB (99.59%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,28 +1,27 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
         n_rows = len(grid)
         n_cols = len(grid[0])
-        visited = set()
         directions = [(1, 0), (0, 1), (-1, 0), (0,-1)]
         num_islands = 0
 
-        def dfs(r, c):
+        def bfs(r, c):
 
-            if (r<0 or r >= n_rows or c<0 or c >= n_cols or grid[r][c] == '0' or (r, c) in visited):
-                return
-            
-            visited.add((r, c))
-
-            for dr, dc in directions:
-                if (r+dr, c+dc) not in visited:
-                    dfs(r+dr, c+dc)
-            
+            queue = deque([(r, c)])
+            while queue:
+                r, c = queue.popleft()
+                for dr, dc in directions:
+                    nr, nc = dr + r, dc + c
+                    if 0 <= nr < n_rows and 0 <= nc < n_cols and grid[nr][nc] == '1':
+                        grid[nr][nc] = 0
+                        queue.append((nr, nc))            
         
         for r in range(n_rows):
             for c in range(n_cols):
-                if grid[r][c] == '1' and (r, c) not in visited:
-                    dfs(r, c)
+                if grid[r][c] == '1':
+                    bfs(r, c)
                     num_islands += 1             
         return num_islands
 
